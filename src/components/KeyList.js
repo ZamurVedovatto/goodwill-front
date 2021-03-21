aeb913f14e8e8fb4d3c16733a002435129b34d9e
Zamur Vedovatto <vedovatto@gmail.com>
2021-03-20 23:59:17 -0300

    adjustments

--- src/components/KeyList.js
+++ src/components/KeyList.js
@@ -1,17 +1,46 @@
 import React from 'react'
-import { Grid, Transition, Icon, Card , Button, Form, Accordion } from 'semantic-ui-react'
-import KeyCard from './../components/KeyCard'
+import { Button, Image, Item } from 'semantic-ui-react'
 
-export default function KeyList({ keys }) {
-  return (
-    <Transition.Group>
-    {
-      keys && keys.map(keyItem => (
-          <Grid.Column key={keyItem.id} style={{ marginBottom: "2rem" }}>
-            <KeyCard keyItem={keyItem} />
-          </Grid.Column>
-        ))
-    }
-  </Transition.Group>
-  )
-}
+const paragraph = <Image src='https://react.semantic-ui.com/images/wireframe/short-paragraph.png' />
+
+const KeyList = () => (
+  <Item.Group relaxed divided>
+    <Item>
+      <Item.Image size='tiny' src='https://react.semantic-ui.com/images/avatar/large/jenny.jpg' />
+
+      <Item.Content verticalAlign='middle'>
+        <Item.Header>Content A</Item.Header>
+        <Item.Description>{paragraph}</Item.Description>
+        <Item.Extra>
+          <Button floated='right'>Action</Button>
+        </Item.Extra>
+      </Item.Content>
+    </Item>
+
+    <Item>
+      <Item.Image size='tiny' src='https://react.semantic-ui.com/images/avatar/large/justen.jpg' />
+
+      <Item.Content verticalAlign='middle'>
+        <Item.Header>Content B</Item.Header>
+        <Item.Description>{paragraph}</Item.Description>
+        <Item.Extra>
+          <Button floated='right'>Action</Button>
+        </Item.Extra>
+      </Item.Content>
+    </Item>
+
+    <Item>
+      <Item.Image size='tiny' src='https://react.semantic-ui.com/images/avatar/large/jenny.jpg' />
+
+      <Item.Content verticalAlign='middle'>
+        <Item.Header>Content C</Item.Header>
+        <Item.Description>{paragraph}</Item.Description>
+        <Item.Extra>
+          <Button floated='right'>Action</Button>
+        </Item.Extra>
+      </Item.Content>
+    </Item>
+  </Item.Group>
+)
+
+export default KeyList
--- src/pages/UserHome.js
+++ src/pages/UserHome.js
@@ -1,6 +1,6 @@
 import React, { useState, useContext, useEffect } from 'react'
 import { useQuery } from "@apollo/client"
-import { Grid, Transition, Icon, Card , Button, Form, Accordion, Container } from 'semantic-ui-react'
+import { Grid, Transition, Icon, Card , Button, Form, Accordion, Container, Segment } from 'semantic-ui-react'
 import { useMutation } from '@apollo/client'
 import PostCard from './../components/PostCard'
 import KeyCard from './../components/KeyCard'
@@ -28,44 +28,45 @@ export default function UserHome() {
     <Container className="container-wrapper">
 
       <Grid columns={1}>
-        <Grid.Column width={6}>
+        <Grid.Column width={7}>
           <Grid.Row>
             {user && (
               <Grid.Column>
                 <Card fluid>
+                <Card.Content extra>
+                  <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-end', justifyContent: 'flex-end'}}>
+                    <Button>
+                      <Icon name='add' />
+                    </Button>
+                  </div>
+                  </Card.Content>
                   <Card.Content>
-                    <Card.Header>Minhas Chaves</Card.Header>
+                    <Card.Header>Keys</Card.Header>
                     <Card.Meta>Texto aqui</Card.Meta>
                     <Card.Description>
                       It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.
                     </Card.Description>
                   </Card.Content>
-                  <Card.Content extra>
-                    <div className='ui two buttons' style={{ display: 'flex', flexDirection: 'column', alignItems: 'start'}}>
-                      <Button color='blue' basic>
-                        <Icon name='add' />
-                      </Button>
-                      <Form>
-                        <Accordion as={Form.Field} panels={panels} />
-                      </Form>
-                    </div>
+                  <Card.Content>
+                    <KeyList />
                   </Card.Content>
                 </Card>
-  
               </Grid.Column>
             )}
           </Grid.Row>
         </Grid.Column>
-        <Grid.Column width={10}>
-          <Grid columns={1}>
-            <Grid.Row>
-              {user && (
-                <Grid.Column>
-                  <PostForm />
-                </Grid.Column>
-              )}
-            </Grid.Row>
-          </Grid>
+        <Grid.Column width={9}>
+          <Segment>
+            <Grid columns={1}>
+              <Grid.Row>
+                {user && (
+                  <Grid.Column>
+                    <PostForm />
+                  </Grid.Column>
+                )}
+              </Grid.Row>
+            </Grid>
+          </Segment>
         </Grid.Column>
       </Grid>
     
